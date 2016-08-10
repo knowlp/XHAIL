c6054cd0237ab2fe1437e063f41627a5665b6204
Peter Schüller <schueller.p@gmail.com>
2016-08-10 16:38:28 +0300

    build and run scripts adapted so that they can be used on multiple systems more easily

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-/home/ps/_setup/gradle-2.10/bin/gradle jar $*
+GRADLE=/home/ps/_setup/gradle-2.10/bin/gradle
+GRADLE=/home/ps/_setup/gradle-2.14.1/bin/gradle
+
+$GRADLE jar $*
--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 #ARGS="--blind -g /usr/bin/gringo -c /home/ps/bin/clasp-3.1.0-x86-linux $* xhail.small.lp"
-ARGS="--blind -g /usr/bin/gringo -c /home/ps/bin/wasp-1c1d45 --budget 300 $* xhail.lp"
+WASP=/home/ps/bin/wasp-1c1d45
+WASP=/home/ps/_setup/wasp/build/release/wasp
+
+ARGS="--blind -g /usr/bin/gringo -c $WASP --budget 300 $* xhail_new.lp"
 
 java -jar /usr/local/xhail-0.5.1/xhail.jar $ARGS
