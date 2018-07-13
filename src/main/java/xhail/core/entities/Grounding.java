20b4bdb191b06e8757f28106c13c7a315bab8d62
eyupogluselin <eyupogluselin@sabanciuniv.edu>
2018-07-13 12:47:50 +0300

    grounding.java calls hypgen.lp for hypotheses generation with mode bias

--- src/main/java/xhail/core/entities/Grounding.java
+++ src/main/java/xhail/core/entities/Grounding.java
@@ -2,21 +2,39 @@
  * 
  */
 package xhail.core.entities;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.TreeMap;
 import java.util.Iterator;
 import java.lang.Math;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.json.* ;
 
 import xhail.core.Buildable;
 import xhail.core.Config;
@@ -36,11 +54,8 @@ import xhail.core.terms.SchemeTerm;
 import xhail.core.terms.Term;
 import xhail.core.terms.Variable;
 
-/**
- * @author stefano
- *
- */
 public class Grounding implements Solvable {
+
 
 	public static class Builder implements Buildable<Grounding> {
 
@@ -131,6 +146,8 @@ public class Grounding implements Solvable {
 		}
 
 	}
+	
+	
 
 	private final Config config;
 
@@ -173,7 +190,8 @@ public class Grounding implements Solvable {
 	public final String asBadSolution() {
 		return String.format("bad_solution:-%snumber_abduced(%d).", count > 0 ? StringUtils.join(delta, ",") + "," : "", count);
 	}
-
+	
+  
 	public String[] asClauses() {
 		Set<String> result = new LinkedHashSet<>();
 		Clause[] clauses = getGeneralisation();
@@ -356,7 +374,264 @@ public class Grounding implements Solvable {
 			result.add(String.format("#show %s/%d.", example.getAtom().getIdentifier(), example.getAtom().getArity()));
 		return result;
 	}
+	/*
+	public final void getRuleFromAnswerset(Object ras, boolean structured, boolean debugCosts)
+	{
+		 
+		  ras = [getTuple(a) for a in ras];
+				  
+		  String head = "";
+		  Map<String,String> pbodies = new HashMap<String,String>();
+		  Map<String,String> nbodies = new HashMap<String,String>();
+		  Map<String,String> variables = new HashMap<String,String>();
 
+
+		  ArrayList<String> predatoms = new ArrayList<String>();
+		  ArrayList<String> varatoms = new ArrayList<String>();
+		  int totalcost = 0;
+		  
+		  Map<Integer,Integer> detailedcosts = new HashMap<Integer,Integer>();
+		  for(atm in ras)
+		  {
+		    apred = atm[0];
+		    if (atm[0].Equals("totalcost"))
+		      totalcost = (int) atm[1];
+		    else if(apred in ['use_head_pred', 'use_body_pred'])
+		      predatoms.append(atm);
+		    else if (apred in ['use_var_type', 'bind_hvar', 'bind_bvar'])
+		      varatoms.append(atm);
+		   else if (apred in ['cost'] and debugCosts)
+		      detailedcosts[atm[1]].append(atm[2:]);
+		  }
+		  
+		  for atm in predatoms:
+		    apred = atm[0]
+		    aargs = atm[1:]
+		    if apred == 'use_head_pred':
+		      predid, pred, arity = aargs
+		      arity = int(arity)
+		      head = [pred] + [None]*arity
+		    elif apred == 'use_body_pred':
+		      id_idx, pred, polarity, arity = aargs
+		      arity = int(arity)
+		      dummy, predid, idx = id_idx
+		      if polarity == 'pos':
+		        bodies = pbodies
+		      else:
+		        bodies = nbodies
+		      bodies[tuple(id_idx)] = [pred] + [None]*arity
+
+		  # get variables
+		  for atm in varatoms:
+		    apred = atm[0]
+		    aargs = atm[1:]
+		    if apred == 'use_var_type':
+		      v_idx, type_ = aargs
+		      v, idx = v_idx
+		      variables[int(idx)] = type_
+		    elif apred == 'bind_hvar':
+		      pos, v_idx = aargs
+		      v, idx = v_idx
+		      head[int(pos)] = makeVar(idx)
+		    elif apred == 'bind_bvar':
+		      id_idx, polarity, position, v_idx = aargs
+		      dummy, predid, idx = id_idx
+		      v, idx = v_idx
+		      if polarity == 'pos':
+		        bodies = pbodies
+		      else:
+		        bodies = nbodies
+		      bodies[tuple(id_idx)][int(position)] = makeVar(idx)
+
+		  safevars = map(makeVarSafe, variables.iteritems())
+		  #warn('safevars:'+repr(safevars))
+
+		  if debugCosts:
+		    #warn('detailedcosts:')
+		    for ct in detailedcosts.iterkeys():
+		      warn('  {}:'.format(ct))
+		      for c in detailedcosts[ct]:
+		        #warn('    '+' | '.join(map(repr, c)))
+		        warn('    '+' | '.join(map(makeAtom, c)))
+
+		  if structured:
+		    # structured output
+		    rule = {
+		      "head": head,
+		      "pbody":pbodies.values() + safevars,
+		      "nbody":nbodies.values()
+		    }
+
+		    return rule, totalcost
+		  else:
+		    # string output
+		    rule = makeAtom(head)
+		    if len(pbodies) + len(safevars) + len(nbodies) == 0:
+		      rule += '.'
+		    else:
+		      lits = sorted([ makeLiteral(lit, True) for lit in (pbodies.values()+safevars) ])
+		      lits += sorted([ makeLiteral(lit, False) for lit in nbodies.values() ])
+		      rule += " :- " + ",".join(lits) + "."
+		    ret = rule, totalcost
+		    if debugCosts:
+		      warn('[{}] {}\n'.format(totalcost, rule))
+		    return ret
+		    		
+	}
+	
+	*/
+    public final List<Entry<JSONObject, JSONObject>> getAnswerSets(String program, String args, boolean withCost) {
+
+    	
+    	//System.out.println(program);
+    	
+    	if(args.equals(""))
+    		args="0";
+    	File temp=null;
+		try {
+			temp = File.createTempFile("tmpf", ".lp");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+    	BufferedWriter bw=null;
+		try {
+			bw = new BufferedWriter(new FileWriter(temp));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    try {
+			bw.write(program);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    try {
+			bw.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    
+        System.out.println("wrote ASP to "+temp.getAbsolutePath());
+        String command= "clingo --outf=2 "+ args + " " +temp.getAbsolutePath();
+        //System.out.println(command);
+        List<String> li= new ArrayList<String>(Arrays.asList(command.split(" ")));
+		ProcessBuilder bld = new ProcessBuilder(li);
+		bld.redirectErrorStream(true);
+        try {
+			Process p = bld.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Runtime rt = Runtime.getRuntime();
+		Process pr=null;
+		try {
+			pr = rt.exec(command);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    BufferedReader stdInput = new BufferedReader(new InputStreamReader(pr.getInputStream()));  
+		String output="";
+		String line="";
+		while (line != null)
+		{
+		output+=line+"\n";
+		try {
+			line=stdInput.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+		//System.out.println("asp output:" + output);
+		JSONObject out=null;
+		try {
+			out = new JSONObject(output);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ArrayList<JSONObject> answersets=null;
+		System.out.println(out);
+       /*
+		JSONArray tmp=null;
+		try {
+			tmp = (JSONArray) out.get("Call");
+		} catch (JSONException e3) {
+			// TODO Auto-generated catch block
+			e3.printStackTrace();
+		}
+		JSONObject obj=null;
+		try {
+			obj = (JSONObject) tmp.get(0);
+		} catch (JSONException e3) {
+			// TODO Auto-generated catch block
+			e3.printStackTrace();
+		}
+		
+		try {
+			obj.get("Witnesses");
+		
+		if((boolean) obj.get("Witnesses"))
+		{
+			try {
+				answersets=(ArrayList<JSONObject>) obj.get("Witnesses");
+			} catch (JSONException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+			try {
+				JSONObject tmp2 = (JSONObject) out.get("Models");
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			//if ((boolean)tmp2.get("Optimal"))
+			     // answersets = answersets[-tmp2.get("Optimal"):];
+			    if (withCost)
+			    {	
+			    	java.util.List<java.util.Map.Entry<JSONObject,JSONObject>> tmp1= new java.util.ArrayList<>();
+			    	for(JSONObject witness : answersets)
+						try {
+							tmp1.add(new java.util.AbstractMap.SimpleEntry(witness.get("Value"), witness.get("cost")));
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+			    	 return tmp1;
+			     // return [ (witness["Value"], witness["Costs"]) for witness in answersets];
+			    		  }
+			    else
+			    {
+			    	java.util.List<java.util.Map.Entry<JSONObject,JSONObject>> tmp1= new java.util.ArrayList<>();
+			    	for(JSONObject witness : answersets)
+						try {
+							tmp1.add(new java.util.AbstractMap.SimpleEntry(witness.get("Value"), 0));
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+			    	 return tmp1;
+			      //return [witness["Value"] for witness in answersets];
+			    }
+		}
+		
+		
+		else
+			return null;
+		} catch (JSONException e3) {
+			// TODO Auto-generated catch block
+			e3.printStackTrace();
+		}
+		*/
+		return null;
+    	
+    }
 	public final Clause[] getGeneralisation() {
 		if (null == generalisation) {
       //Logger.message("getGeneralization");
@@ -410,17 +685,131 @@ public class Grounding implements Solvable {
 		return generalisation;
 	}
 
+	
 	public final Clause[] getKernel() {
 		if (null == kernel) {
 			//Logger.message("getKernel");
+			
 			Set<Clause> set = new LinkedHashSet<>();
+			//
+		
+			String insp="";
+				
+			/*
+			 * % head predicates and args
+				hpred(I,P,N) :- tpred(I,P,N).
+				harg(I,J,T) :- targ(I,J,T).
+
+				% body predicates and args
+				% bpred(PredIdentifier,Predicate,NumberOfArguments)
+				% barg(PredIdentifier,ArgumentPosition,Type)
+				bpred(I,P,N) :- rpred(I,P,N).
+				barg(I,J,T) :- rarg(I,J,T).
+				
+				#modeh flies(+bird).
+                #modeb penguin(+bird).
+				#modeb not penguin(+bird).
+			 */
+			
+			String modeH=null;
+			String headt=null;
+			String htmp= null;
+			String[] hargs=null;
+			int counter=0;
+			for(int i=0;i<getModeHs().length;i++)
+			{
+				modeH = getModeHs()[i].toString();
+				if(modeH.contains("not"))
+					htmp = modeH.split(" ")[2].toString();
+				else
+					htmp = modeH.split(" ")[1].toString();
+				headt = htmp.split("\\(")[0];
+			    hargs = htmp.split("\\(")[1].split(",");
+			    if(modeH.contains("not"))
+			    	insp+="not tpred("+ counter + "," + headt + "," + hargs.length +").\n";
+			    else
+			    	insp+="tpred("+ counter + "," + headt + "," + hargs.length +").\n";
+				hargs[0]=hargs[0].replaceAll("[^A-Za-z]+", "");
+				hargs[hargs.length-1]=hargs[hargs.length-1].replaceAll("[^A-Za-z]+", "");
+				
+				for(int k=1;k<hargs.length;k++)
+					{insp+="targ("+ counter + "," + k + "," + hargs[k] +").\n";}
+				counter+=1;
+			}
+			
+			insp+="\n";
+			String modeB=null;
+			String body=null;
+			String btmp= null;
+			String[] bargs=null;
+			for(int i=0;i<getModeBs().length;i++)
+				{
+				modeB = getModeBs()[i].toString();
+				if(modeB.contains("not"))
+					btmp = modeB.split(" ")[2].toString();
+				else
+					btmp = modeB.split(" ")[1].toString();
+				body = btmp.split("\\(")[0];
+				bargs = btmp.split("\\(")[1].split(",");
+				if(modeB.contains("not"))
+					insp+="not rpred("+ counter + "," + body + "," + bargs.length +").\n";
+				else
+					insp+="rpred("+ counter + "," + body + "," + bargs.length +").\n";
+				bargs[0]=bargs[0].replaceAll("[^A-Za-z]+", "");
+				bargs[bargs.length-1]=bargs[bargs.length-1].replaceAll("[^A-Za-z]+", "");
+				for(int j=1;j<bargs.length;j++)
+					insp+="rarg("+ counter + "," + j + "," + bargs[j] +").\n";
+				counter+=1;
+				}
+			
+			insp+="\n";
+			
+			//System.out.println(insp);
+			int costlimit=4;
+			String line="";
+			
+		    FileReader fr=null;
+			try {
+				fr = new FileReader("hypgen.lp");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			BufferedReader br = new BufferedReader(fr);
+			try {
+				while ((line = br.readLine()) != null) 
+					insp+=line+"\n";
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			List<Map.Entry<JSONObject,JSONObject>> hyp_as = getAnswerSets(insp,"--const maxcost="+costlimit+" 1",false);
+			
+			/*
+			String[] allrules = null;
+			String[] candidateRules;
+				
+			for(Object ras : hyp_as)
+			{
+			   if(true)
+		        {
+		          int nsrule, nscost = getRuleFromAnswerset(ras, structured=False, debugCosts=False);
+		        }
+				int rule, cost = getRuleFromAnswerset(ras, structured=True);
+				allrules.append(Pair.of(rule.toString(),cost.toString)));
+			}  
+			
+			//
+	*/
 			for (Atom alpha : delta)
 				for (ModeH head : problem.getModeHs()) {
 					Scheme scheme = head.getScheme();
 					if (SchemeTerm.subsumes(scheme, alpha, facts)) {
 						Clause.Builder builder = new Clause.Builder().setHead(//
-								new Atom.Builder(alpha).setWeight(head.getWeigth()).setPriority(head.getPriority()).build());
-
+								new Atom.Builder(alpha).setWeight(head.getWeigth()).setPriority(head.getPriority()).build());//uses examples
+                        
 						Collection<Term> substitutes = SchemeTerm.findSubstitutes(scheme, alpha);
 						if (null != substitutes) {
 							int level = 0;
@@ -435,7 +824,7 @@ public class Grounding implements Solvable {
 										Map<Atom, Collection<Term>> found = SchemeTerm.generateAndOutput(scheme, usables, table, facts);
 										for (Atom atom : found.keySet()) {
 											builder.addLiteral(new Literal.Builder( //
-													new Atom.Builder(atom).setWeight(mode.getWeigth()).setPriority(mode.getPriority()).build() //
+													new Atom.Builder(atom).setWeight(mode.getWeigth()).setPriority(mode.getPriority()).build() //uses examples
 											).setNegated(mode.isNegated()).setLevel(level).build());
 											next.addAll(found.get(atom));
 										}
@@ -443,7 +832,7 @@ public class Grounding implements Solvable {
 										Map.Entry<Collection<Atom>, Collection<Term>> found = SchemeTerm.matchAndOutput(scheme, table.get(scheme), usables);
 										for (Atom atom : found.getKey())
 											builder.addLiteral(new Literal.Builder( //
-													new Atom.Builder(atom).setWeight(mode.getWeigth()).setPriority(mode.getPriority()).build() //
+													new Atom.Builder(atom).setWeight(mode.getWeigth()).setPriority(mode.getPriority()).build() //uses examples
 											).setNegated(mode.isNegated()).setLevel(level).build());
 										next.addAll(found.getValue());
 									}
@@ -456,9 +845,11 @@ public class Grounding implements Solvable {
 							}
 						}
 						set.add(builder.build());
+						kernel = set.toArray(new Clause[set.size()]);
 					}
 				}
 			kernel = set.toArray(new Clause[set.size()]);
+		
 		}
 		return kernel;
 	}
@@ -467,6 +858,10 @@ public class Grounding implements Solvable {
 		return problem.getModeBs();
 	}
 
+	public final String[] getBackg() {
+		return problem.getBackground();
+	} 
+	
 	public final ModeH[] getModeHs() {
 		return problem.getModeHs();
 	}
